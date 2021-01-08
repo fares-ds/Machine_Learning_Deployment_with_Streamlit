fd8c51b6812fcc29a6b6bac95d8db61ba6c60d12
Fares Sayah <sayahfares19@gmail.com>
2021-01-09 00:31:24 +0100

    Upload ml models + tfidf + make prediction on user input

--- ds_web_page.py
+++ ds_web_page.py
@@ -1,9 +1,27 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt 
-import seaborn as sns
-from collections import Counter
+import pickle
+
+# print('Uploading ML models')
+lr_model = pickle.load(open('models/Logistic_Regression.sav', 'rb'))
+svm_model = pickle.load(open('models/Support_Vector_Machine.sav', 'rb'))
+nb_model = pickle.load(open('models/Naive_Bayes.sav', 'rb'))
+# print('ML models upload successfully....')
+
+# print('Uploading Tfidf vectorizer models')
+tfidf = pickle.load(open('models/tfidf.sav', 'rb'))
+# print('Tfidf vectorizer upload successfully....')
+
+def predict_tweet(model, text):
+	vectorize_text = tfidf.transform([text])
+	target = model.predict(vectorize_text)[0]
+	return target
+
+def target_value(target):
+	if target:
+		return 'Disater'
+	return 'Not Disaster'
 
 st.title('Disaster Tweets Classification')
 st.markdown("""
@@ -18,13 +36,14 @@ This app performs simple classification of tweet into disaster or Not.
 """)
 
 # Input Text In a box
-st.header('Enter Your text: ')
-text_input = '>Text Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT'
-
+st.header('Enter Your tweet here: ')
+text_input = ''
 text = st.text_area('Text input', text_input, height=250)
-text = text.splitlines()
-text = text[1:]
-text = ''.join(text)
+
+lr_result = target_value(predict_tweet(lr_model, text))
+svm_result = target_value(predict_tweet(svm_model, text))
+nb_result = target_value(predict_tweet(nb_model, text))
+
 
 st.write("""
 ***
@@ -35,12 +54,11 @@ st.header('INPUT TEXT: ')
 text
 
 # Text words count
-st.header('OUTPUT (NUMBER OF WORDS): ')
-words_count = len(text.split())
-words_count
+st.header('Logistic Regression: ')
+lr_result
 
-# Word counter
-st.header('OUTPUT (WORDS COUNT)')
-count = Counter()
-count.update(text.split())
-count
+st.header('Support Vector Machine: ')
+svm_result
+
+st.header('Naive Bayes: ')
+nb_result
